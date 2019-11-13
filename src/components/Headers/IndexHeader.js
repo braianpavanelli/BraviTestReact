60c0c18cb0b544b379889485f2b637333dfb7290
Braian Pavanelli <braian.pavanelli@consultsolucoes.com.br>
2019-11-13 00:51:25 -0300

    InitialLayouted

---
+++ src/components/Headers/IndexHeader.js
@@ -0,0 +1,73 @@
+/*eslint-disable*/
+import React from "react";
+
+// reactstrap components
+import { Container } from "reactstrap";
+// core components
+
+function IndexHeader() {
+  let pageHeader = React.createRef();
+
+  React.useEffect(() => {
+    if (window.innerWidth > 991) {
+      const updateScroll = () => {
+        let windowScrollTop = window.pageYOffset / 3;
+        pageHeader.current.style.transform =
+          "translate3d(0," + windowScrollTop + "px,0)";
+      };
+      window.addEventListener("scroll", updateScroll);
+      return function cleanup() {
+        window.removeEventListener("scroll", updateScroll);
+      };
+    }
+  });
+
+  return (
+    <>
+      <div className="page-header clear-filter" filter-color="blue">
+        <div
+          className="page-header-image"
+          style={{
+            backgroundImage: "url(" + require("../../img/woman-phone.jpg") + ")"
+          }}
+          ref={pageHeader}
+        ></div>
+        <Container>
+          <div className="content-center brand">
+            <img
+              alt="..."
+              className="n-logo"
+              src={require("../../img/logo-bravi.png")}
+            ></img>
+            <h1 className="h1-seo">Test Braian Pavanelli</h1>
+            <h3>This Layout is Now-Ui-Kit by Creative Tim</h3>
+          </div>
+          <h6 className="category category-absolute">
+            Designed by{" "}
+            <a href="http://invisionapp.com/?ref=creativetim" target="_blank">
+              <img
+                alt="..."
+                className="invision-logo"
+                src={require("../../img/invision-white-slim.png")}
+              ></img>
+            </a>
+            . Coded by{" "}
+            <a
+              href="https://www.creative-tim.com?ref=nukr-index-header"
+              target="_blank"
+            >
+              <img
+                alt="..."
+                className="creative-tim-logo"
+                src={require("../../img/creative-tim-white-slim2.png")}
+              ></img>
+            </a>
+            .
+          </h6>
+        </Container>
+      </div>
+    </>
+  );
+}
+
+export default IndexHeader;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "./css/bootstrap.min.css";
+import "./css/now-ui-kit.css";
 import './index.css';
+import Index from "./views/Index.js";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<Index />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
